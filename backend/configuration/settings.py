ffab5adbba100aa751efab48f7eb7b6b0c163545
Ioannis Nezis <ioannis@nezis.de>
2025-10-18 21:33:22 +0200

    CI: mount db

--- backend/configuration/settings.py
+++ backend/configuration/settings.py
@@ -23,12 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z^-pm8zwktx+ma1kt^pbv&6deb^lp=sxt*1j6tilp^_2at9^-u"
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET", "notsercure")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
