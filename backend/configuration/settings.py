3088d6ef198c103f833d039045b45fce1af6eb6e
Ioannis Nezis <ioannis@nezis.de>
2025-10-19 19:04:32 +0200

    ENH: serve static files for api

--- backend/configuration/settings.py
+++ backend/configuration/settings.py
@@ -27,9 +27,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET", "notsercure")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
+
 ALLOWED_HOSTS = ["*"]
 
-USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_HOST = not DEBUG or (os.environ.get("IS_PROXIED", "False") == "True")
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,6 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
