413f5deaf68f3e17d8c2b6cd203869a1367b0910
Ioannis Nezis <ioannis@nezis.de>
2025-10-20 13:51:46 +0200

    MAINT: formatting

--- frontend/src/theme_switcher.ts
+++ frontend/src/theme_switcher.ts
@@ -1,15 +1,20 @@
 export function setup_theme_switcher() {
   let darkMode = false;
-  if (localStorage.getItem("theme") === "dark") { darkMode = true }
-  else if (localStorage.getItem("theme") === "light") { darkMode = false }
-  if (darkMode) { document.documentElement.classList.toggle("dark") }
-  var themeSwitch = document.querySelectorAll("[data-theme-switcher]");
-  const themeSwitcher = document.getElementById("theme-switcher")! as HTMLInputElement;
-  themeSwitcher.addEventListener("change", () => {
-    document.documentElement.classList.toggle("dark");
+  if (localStorage.getItem('theme') === 'dark') {
+    darkMode = true;
+  } else if (localStorage.getItem('theme') === 'light') {
+    darkMode = false;
+  }
+  if (darkMode) {
+    document.documentElement.classList.toggle('dark');
+  }
+  var themeSwitch = document.querySelectorAll('[data-theme-switcher]');
+  const themeSwitcher = document.getElementById('theme-switcher')! as HTMLInputElement;
+  themeSwitcher.addEventListener('change', () => {
+    document.documentElement.classList.toggle('dark');
     localStorage.setItem(
-      "theme",
-      document.documentElement.classList.contains("dark") ? "dark" : "light"
+      'theme',
+      document.documentElement.classList.contains('dark') ? 'dark' : 'light'
     );
   });
 }
