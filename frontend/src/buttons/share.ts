bda9b9bd0caad52ea926351f483958baae4cff21
Ioannis Nezis <ioannis@nezis.de>
2025-11-08 01:06:43 +0100

    WIP: share links

--- frontend/src/buttons/share.ts
+++ frontend/src/buttons/share.ts
@@ -1,23 +1,54 @@
+import type { Backend } from '../types/backend';
 import type { EditorAndLanguageClient } from '../types/monaco';
 import { getPathParameters } from '../utils';
 
-export function setup(editorAndLanguageClient: EditorAndLanguageClient) {
+export async function setup(editorAndLanguageClient: EditorAndLanguageClient) {
   const shareButton = document.getElementById('shareButton')!;
   const shareModal = document.getElementById('shareModal')!;
   const share = document.getElementById('share')!;
   const shareLink3 = document.getElementById('shareLink3')!;
+  const shareLink4 = document.getElementById('shareLink4')!;
+  const shareLink5 = document.getElementById('shareLink5')!;
+  const shareLink6 = document.getElementById('shareLink6')!;
 
+  shareButton.addEventListener('click', async () => {
 
-  shareButton.addEventListener('click', () => {
-    shareModal.classList.remove('hidden');
     const query = editorAndLanguageClient.editorApp.getEditor()!.getValue();
-    const [slug, _] = getPathParameters();
-    const url = new URL(window.location.origin)
-    url.pathname = slug!;
-    url.searchParams.set("query", encodeURIComponent(query));
-    shareLink3.textContent = url.toString();
 
-    // editorAndLanguageClient.languageClient.sendRequest()
+    if (query.trim() === "") {
+      window.dispatchEvent(new CustomEvent('toast', {
+        detail: {
+          type: "warning",
+          message: "There is nothing to share.",
+          duration: 3000
+        }
+      }));
+      return
+    }
+
+    shareModal.classList.remove('hidden');
+
+    const [slug, _] = getPathParameters();
+    const backend = await editorAndLanguageClient.languageClient.sendRequest("qlueLs/getBackend", {}) as Backend;
+
+    // NOTE: URL to this query in the QLever UI (long, with full query string) 
+    const url1 = new URL(window.location.origin)
+    url1.pathname = slug!;
+    url1.searchParams.set("query", encodeURIComponent(query));
+    shareLink3.textContent = url1.toString();
+
+    // NOTE: URL for GET request (for use in web apps, etc.)
+    const url2 = new URL(backend.url);
+    url2.searchParams.set("query", encodeURIComponent(query));
+    shareLink4.textContent = url2.toString();
+
+    // NOTE: cURL command line for POST request (application/sparql-results+json): 
+    const normalized = query.replace(/\s+/g, " ").trim();
+    const escaped = normalized.replace(/"/g, '\\"');
+    shareLink5.textContent = `curl -s ${backend.url} -H "Accept: application/sparql-results+json" -H "Content-type: application/sparql-query" --data "${escaped}"`
+
+    // NOTE:  cURL command line for GET request (application/qlever-results+json): 
+    shareLink6.textContent = `curl -s ${backend.url} -H "Accept: application/qlever-results+json" --data-urlencode "query=${escaped}"`;
 
   });
   shareModal.addEventListener('click', () => {
